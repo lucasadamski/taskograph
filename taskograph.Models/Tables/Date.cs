27844137be4c5bbe246714290dce60a34829b57f
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2024-03-19 14:01:35 +0000

    Created Tables and Columns Descriptions

---
+++ taskograph.Models/Tables/Date.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskograph.Models.Tables
+{
+    public class Date
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public DateTime? Deleted { get; set; }
+    }
+}
