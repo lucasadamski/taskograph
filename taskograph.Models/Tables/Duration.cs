108f8d4ce54893ba05aa5acca4ed1403baa7255a
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2024-03-30 08:31:52 +0000

    Added PrepareText to Duration Model

--- taskograph.Models/Tables/Duration.cs
+++ taskograph.Models/Tables/Duration.cs
@@ -20,5 +20,23 @@ namespace taskograph.Models.Tables
         public ICollection<RegularTarget> TargetRegularTargets { get; set; }
         [InverseProperty(nameof(RegularTarget.PerTimeframeDuration))]
         public ICollection<RegularTarget> PerTimeframeRegularTargets { get; set; }
+
+        public string PrepareText()
+        {
+            if (Months != null) 
+                return $"{Months} months";
+            else if (Weeks != null) 
+                return $"{Weeks} weeks";
+            else if (Days != null) 
+                return $"{Days}d";
+            else if (Hours != null && Minutes != null)
+                return String.Format("{0:00}:{1:00}", Hours, Minutes);
+            else if (Hours != null && Minutes == null) 
+                return String.Format("{0:00}:00", Hours);
+            else if (Hours == null && Minutes != null) 
+                return String.Format("00:{0:00}", Minutes);
+            else 
+                return "Empty";
+        }
     }
 }
