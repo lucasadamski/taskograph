880e908dcd29b06be27b71a1db9458e723923fa1
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2024-03-31 19:01:19 +0100

    Added TaskDTO

---
+++ taskograph.Web/Models/DTOs/TaskDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace taskograph.Web.Models.DTOs
+{
+    public class TaskDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public string Color { get; set; }
+        public string TotalDurationToday { get; set; }
+    }
+}
