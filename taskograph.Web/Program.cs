1357425f6ece2634a821b79856714b2a8f6a31fe
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2024-04-04 12:31:06 +0100

    Implemented NLOG, Waring: Change log path in nlog.config

--- taskograph.Web/Program.cs
+++ taskograph.Web/Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
 using taskograph.EF.DataAccess;
 using taskograph.EF.Repositories;
 using taskograph.EF.Repositories.Infrastructure;
@@ -11,50 +12,63 @@ namespace taskograph.Web
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<TasksContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<TasksContext>();
-            builder.Services.AddScoped<ITaskRepository, TaskRepository>();
-            builder.Services.AddScoped<IEntryRepository, EntryRepository>();
-            builder.Services.AddScoped<IDurationRepository, DurationRepository>();
-            builder.Services.AddScoped<IPreciseTargetRepository, PreciseTargetRepository>();
-            builder.Services.AddScoped<IRegularTargetRepository, RegularTargetRepository>();
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+            logger.Debug("init main");
+            // try catch for logging purposes
+            try
             {
-                app.UseMigrationsEndPoint();
+                var builder = WebApplication.CreateBuilder(args);
+                // Add services to the container.
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+                builder.Services.AddDbContext<TasksContext>(options =>
+                    options.UseSqlServer(connectionString));
+                builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+                builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<TasksContext>();
+                builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+                builder.Services.AddScoped<IEntryRepository, EntryRepository>();
+                builder.Services.AddScoped<IDurationRepository, DurationRepository>();
+                builder.Services.AddScoped<IPreciseTargetRepository, PreciseTargetRepository>();
+                builder.Services.AddScoped<IRegularTargetRepository, RegularTargetRepository>();
+                builder.Services.AddControllersWithViews();
+                builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+                // NLog: Setup NLog for Dependency injection
+                builder.Logging.ClearProviders();
+                builder.Host.UseNLog();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseMigrationsEndPoint();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+                app.UseRouting();
+                app.UseAuthorization();
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                app.MapRazorPages();
+                app.Run();
             }
-            else
+            catch (Exception e)
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                logger.Error($"Program.cs: Message: {e.Message}");
+                throw;
             }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
-
-            app.Run();
+            finally
+            {
+                LogManager.Shutdown();
+            }
         }
     }
 }
