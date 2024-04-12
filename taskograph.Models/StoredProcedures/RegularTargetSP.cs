54290e367e7bcfb42e20beb7946f20ff1cdde7f6
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2024-04-12 10:47:22 +0100

    Added RegularTargetSP It's a return model from spGetRegularTargets and spGetAllRegularTargets stored procedures.

---
+++ taskograph.Models/StoredProcedures/RegularTargetSP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace taskograph.Models.StoredProcedures
+{
+    public class RegularTargetSP
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public long TargetDuration { get; set; }
+        public long PerTimeframeDuration { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public DateTime? Deleted { get; set; }
+
+    }
+}
