4c0920caff9a02e50bfc0cc18a20110816956a64
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2024-04-12 10:49:44 +0100

    Added RegularTargetDTO It will be mapped from RegularTargetSP and passed to RegularTargetsController

---
+++ taskograph.Models/DTOs/RegularTargetDTO.cs
@@ -0,0 +1,17 @@
+﻿using taskograph.Models.Tables;
+
+namespace taskograph.Models.DTOs
+{
+    public class RegularTargetDTO
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public Duration TargetDuration { get; set; }
+        public Duration PerTimeframeDuration { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public DateTime? Deleted { get; set; }
+
+    }
+}
+}
