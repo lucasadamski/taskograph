44766bf62ca4fed8386a150305403db8e6fec4b8
Lucas <lukaszadamczy@gmail.com>
2024-08-23 21:17:50 +0100

    Added DbContext mocking

--- taskograph.DBTests/TaskRepositoryTests.cs
+++ taskograph.DBTests/TaskRepositoryTests.cs
@@ -1,11 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using taskograph.EF.DataAccess;
+using Task = taskograph.Models.Tables.Task;
+
 namespace taskograph.DBTests
 {
     public class TaskRepositoryTests
     {
-        [Fact]
-        public void Test1()
+       private async Task<TasksContext> GetDbContext()
         {
+            var options = new DbContextOptionsBuilder<TasksContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) //in memory nuget necessary
+                .Options;
+            //creating DBcontext from mock
+            var dbContext = new TasksContext(options);
+            dbContext.Database.EnsureCreated();
 
+            //filling with seed data
+            if (await dbContext.Tasks.CountAsync() < 0)
+            {
+                dbContext.Tasks.Add(new Task { Id = 1, Name = "Running", GroupId = 4, Created = DateTime.Now, ApplicationUserId = "none" });
+                dbContext.Tasks.Add(new Task { Id = 2, Name = "Cooking", GroupId = 4, Created = DateTime.Now, ApplicationUserId = "none" });
+                dbContext.Tasks.Add(new Task { Id = 3, Name = "Reading", GroupId = 4, Created = DateTime.Now, ApplicationUserId = "none" });
+                await dbContext.SaveChangesAsync();
+            }
+            return dbContext;
         }
     }
 }
